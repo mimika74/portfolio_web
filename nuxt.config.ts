dfdbcb0068fe8f9f3a21c0f20eff127975fd4d94
mimika74 <thfoeichir74mcs@gmail.com>
2026-09-23 23:32:02 +0900

    [delete] favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,7 +57,6 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
     },
   },
